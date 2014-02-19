9fa16de76b71b6d658a101f717209e7d17f7d92c
Gaia Silvestri <gaiasilvestri00@gmail.com>
2014-02-19 15:21:03 -0800

    moved text for the views to the controllers

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,9 +3,13 @@
 /* Controllers */
 
 angular.module('myApp.controllers', []).
-  controller('MyCtrl1', [function() {
-
+  controller('MyCtrl1', ['$scope', function($scope) {
+    $scope.titlebox1 = 'HTML';
+    $scope.titlebox2 = 'CSS';
+    $scope.titlebox3 = 'JavaScript';
   }])
-  .controller('MyCtrl2', [function() {
+  .controller('MyCtrl2', ['$scope', function($scope) {
+  	$scope.greeting = 'Welcome to the gallery!';
+  	$scope.gallerysubtitle = 'Click on a creation to see the code in action.';
 
   }]);
